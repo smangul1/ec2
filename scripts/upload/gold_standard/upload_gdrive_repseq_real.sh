b0190b3f28925639bab8743f4eb99271ba4c5783
Brian Hill <brian.l.hill11@gmail.com>
2017-12-26 18:50:22 -0800

    Added scripts for uploading data to Google drive

---
+++ scripts/upload/gold_standard/upload_gdrive_repseq_real.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/u/home/s/serghei/project/code/bin/gdrive upload -r -p 1b8WBIpCWZXgF16z9RC-Hi-3jtGaPIbCD /u/home/n/ngcrawfo/project-zarlab/igor/VIRAL_CORRECTION/tools/data/datasets_no_barcodes_no_index/for_paper/without_barcode
---
+++ scripts/upload/raw_data/upload_gdrive_wgs_sim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/u/home/s/serghei/project/code/bin/gdrive upload -r -p 1ZLII-Jv_LJT7n5W_Fd5RNMOuovvDeNqZ /u/home/s/serghei/project/EC_survey/WGS_sim
