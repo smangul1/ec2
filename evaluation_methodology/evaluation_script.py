228ef0702bba24ea7785888a6348024ae09972d7
Keith <keithgeraldmitchell@gmail.com>
2018-03-14 20:54:21 -0700

    post meeting msa hack

--- evaluation_methodology/evaluation_script.py
+++ evaluation_methodology/evaluation_script.py
@@ -1,102 +1,105 @@
 
+######################################################################
+###Evaluation Code for Error Correction Benchmarking
+#  project zar-lab ucla
+#  3/14/18
+
+#  Functions Contained: analyze_corrections,
+######################################################################
+
+#small testing code below the functions feel free to try to play around with it.
+
 
 from Bio.Alphabet import generic_dna
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
-from Bio.Align.Applications import MuscleCommandline
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 
 
-######################################################################
-###Evaluation Script Methodology
-######################################################################
-# True and Raw Sequences are all the same to compare to for all of the EC reads from the seperate tools.
-# This might be a list of files since there are multiple data sets to evaluate.
-#     true_file = "file_path.fastq"[]
-#     raw_file = "file_path.fastq"[]
+def analyze_corrections(true_sequence, raw_sequence, ec_sequence):
+    """Keith Mitchell (keithgmitchell@g.ucla.edu).
+        Function: Analyze true, raw(error_prone), and flawed/flawless error corrected reads
+        Input 1: (string) true_sequence to be compared to raw and tool error corrected seq.
+        Input 2: (string) raw_sequence (error prone sequence) that is to be compare to true and error corrected seq.
+        Input 3: (string) ec_sequence (error corrected sequence) to be analyzed for flawed/flawless error correction
+        Returns:
+            Object 1: (dictionary) stats_dict which has the base level counts for the sequence:
+                EX:        stats_dict = {'TN':0, 'TP':0, 'FN':0, 'FP':0, 'INDEL':0, 'TRIM': 0}
+            Object 2: (string) seq_ID which dictates the read level classification (one of the following list):
+                EX:        ["TN", "FP(NORMAL)", "FP(INDEL)", "FP(TRIMMING)", "TP", "FN"]
+    """
+    alignments = pairwise2.align.globalms(true_sequence, raw_sequence, 5, -4, -3, -0.1)
 
-# Given those are the same we just need to iterate through the ec files and compare to the corresponding raw/true file.
-#     RUSSEL see what you can do here
-#     need to be sure we know which tool each EC file is from
-#     create temp. fasta file with the three sequences in it.
+    true_aligned_raw = alignments[0][0]
+    raw_aligned_true = alignments[0][1]
 
-# Perform MSA using muscle to create the align object using the fasta file created in the previous step.
+    alignments_2 = pairwise2.align.globalms(true_aligned_raw, ec_sequence, 5, -4, -3, -0.1)
 
-# Use MSA to analyze the TN, TP, FP, FN for each read.
+    true_aligned_raw_ec = alignments_2[0][0]
+    ec_aligned_true = alignments_2[0][1]
 
-# Aggregate the results from each read into a data structure that can easily incorporate into excel.
+    alignments_3 = pairwise2.align.globalms(ec_aligned_true, raw_aligned_true, 5, -4, -3, -0.1)
 
-# (might be beneficial to make some of the above steps into definitions in the script.)
+    ec_aligned_final = alignments_2[0][0]
+    raw_aligned_final = alignments_2[0][1]
 
-######################################################################
+    print "True:", true_aligned_raw_ec
+    print "Raw: ", raw_aligned_final
+    print "EC:  ", ec_aligned_final
 
+    stats_dict = {'TN':0, 'TP':0, 'FN':0, 'FP':0, 'INDEL':0, 'TRIM': 0}
 
-### True and Raw Sequences are all the same to compare to for all of the EC reads from the seperate tools.
-### Given those are the same we just need to iterate through the ec files and compare to the corresponding raw/true file.
-
-#for dataset in datasets:
-    #for tool in tools:
-
-tool_name = ""
-true = SeqRecord(Seq("", generic_dna), id="Alpha")
-raw = SeqRecord(Seq("", generic_dna), id="Beta")
-ec = SeqRecord(Seq("", generic_dna), id="Gamma")
-
-#compile these three sequences of interest into a temp. file called unaligned.fasta
-
-#can add annotations to sequences with tool name/dataset name here potentially using seqrecord library object.
-
-
-
-
-### Perform MSA using muscle to create the align object using the fasta file created in the previous step.
-
-muscle_exe = r"C:\Program Files\Aligments\muscle3.8.31_i86win32.exe"
-in_file = "..\unaligned.fasta"
-out_file = "..\aligned.fasta"
-muscle_cline = MuscleCommandline(muscle_exe, input=in_file, out=out_file)
-print(muscle_cline)
-"C:\Program Files\Aligments\muscle3.8.31_i86win32.exe" -in "C:\My Documents\unaligned.fasta" -out "C:\My Documents\aligned.fasta"
+    if len(true_aligned_raw_ec) == len(ec_aligned_final) == len(raw_aligned_final):
+        for true_bp, raw_bp, ec_bp in zip(true_aligned_raw_ec, raw_aligned_final, ec_aligned_final):
+            if true_bp == raw_bp == ec_bp:
+                stats_dict['TN'] += 1
+            elif true_bp == raw_bp != ec_bp:
+                stats_dict['FP'] += 1
+            elif true_bp != raw_bp != ec_bp and ec_bp == true_bp:
+                stats_dict['TP'] += 1
+            elif true_bp != raw_bp == ec_bp:
+                stats_dict['FN'] += 1
+            if (true_bp == "-") or (raw_bp == "-") or (ec_bp == "-"):
+                stats_dict['INDEL'] += 1
 
 
+        ##this portion decides what to classify the sequence as a whole as.
 
+        seq_classes = ["TN", "FP(NORMAL)", "FP(INDEL)", "FP(TRIMMING)", "TP", "FN"]
+        seq_ID = ""
+        if stats_dict['TN'] == len(true_aligned_raw_ec):
+            seq_ID = seq_classes[0]
+        elif stats_dict['FP'] != 0 and stats_dict['TP'] != 0 and stats_dict['INDEL'] == 0:
+            seq_ID = seq_classes[1]
+        elif stats_dict['FP'] != 0 and stats_dict['INDEL'] != 0:
+            seq_ID = seq_classes[2]
+        elif stats_dict['TP'] != 0 and stats_dict['FP'] == 0:
+            seq_ID = seq_classes[4]
 
-### Use MSA to analyze the TN, TP, FP, FN for each read using the "aligned.fasta" file
+        #what to do about number 7, also need to incorporate the "FN"
 
-TN, TP, FN, FP, INDEL = 0
-#need to also factor in trimming here somehow
+        print "BASE LEVEL: ", stats_dict
+        print "READ LEVEL: ", seq_ID
 
-for true_bp, raw_bp, ec_bp in align[0], align[1], align[2]:
-    if true_bp == raw_bp == ec_bp:
-        TN +=1
-    elif true_bp == raw_bp != ec_bp:
-        FP +=1
-    elif true_bp != raw_bp != ec_bp and ec_bp == true_bp:
-        TP +=1
-    elif true_bp != raw_bp == ec_bp:
-        FN +=1
-    elif true_bp or raw_bp or ec_bp == "-":
-        INDEL += 1
+    else:
+        print true_aligned_raw_ec, raw_aligned_final, ec_aligned_final
 
-seq_classes = ["TN", "FP(NORMAL)", "FP(INDEL)", "FP(TRIMMING)", "TP", "FN"]
-seq_ID = ""
-
-if TN == len(align):
-    seq_ID = seq_classes[0]
-elif FP != 0 and TP != 0 and INDEL == 0:
-    seq_ID = seq_classes[1]
-elif FP != 0 and TP != 0 and INDEL != 0:
-    seq_ID = seq_classes[2]
-elif TP != 0 and FP == 0:
-    seq_ID = seq_classes[4]
-#what to do about number 7, also need to incorporate the "FN"
-
-
-
-
-
+    return stats_dict, seq_ID
 
 ### Aggregate the results from each read into a data structure that can easily incorporate into excel.
+### using the tool, dataset, and sequence ID.
+
+
+
+
+true_sequence = 'ACCGGTCCA'
+raw_sequence =  'ACCGGTCCA'
+ec_sequence =   'CCGTGTCC'
+
+analyze_corrections(true_sequence, raw_sequence, ec_sequence)
+
 
 
 
